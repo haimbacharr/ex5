a42ad666df79f95865fb9dfd3abde49b97b3190d
haimbacharr <81071510+haimbacharr@users.noreply.github.com>
2022-04-28 00:21:50 +0300

    Initial commit

---
+++ app/src/main/java/com/example/ex5/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.ex5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements FragA.FragAListener, FragB.FragBListener {
+    FragA fragA;
+    FragB fragB;
+
+
+    static float op1, op2, initial_result;
+    static int zeroCnt = 0;
+
+    int duration = Toast.LENGTH_SHORT;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            fragA = (FragA) getSupportFragmentManager().findFragmentByTag("FRAG1");
+            fragB = (FragB) getSupportFragmentManager().findFragmentByTag("FRAG2");
+
+            if ((getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)){
+                if (fragB != null) {
+                    getSupportFragmentManager().beginTransaction()
+                            .show(fragB)
+                            .addToBackStack(null)
+                            .commit();
+                }
+                else {
+                    getSupportFragmentManager().beginTransaction()
+                            .add(R.id.fragContainer, FragB.class,null, "FRAGB")
+                            .commit();
+                }
+                getSupportFragmentManager().executePendingTransactions();
+            }
+    }
+
+
+    public void OnClickEvent(View view, String ed1, String ed2){
+        op1 = Float.parseFloat(ed1);
+        op2 = Float.parseFloat(ed2);
+        Log.i(" on click Fram Main: %s", String.valueOf(op1));
+        Log.i("%s !!!!!!!!!!!!!!!!", String.valueOf(op2));
+        Button btn = findViewById(((Button)view).getId());
+
+        if (btn.getText().toString().equals("/")) {
+               if (!div_check(op2)) {
+                    return;
+               }
+        }
+        int btId = ((Button)view).getId();
+            if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                getSupportFragmentManager().beginTransaction()
+                        .setReorderingAllowed(true)
+                        .replace(R.id.fragContainer, FragB.class, null, "FRAG2")
+                        .addToBackStack("BBB")
+                        .commit();
+                getSupportFragmentManager().executePendingTransactions();
+                fragB = (FragB) getSupportFragmentManager().findFragmentByTag("FRAG2");
+            }
+            else{
+                fragB = (FragB)getSupportFragmentManager().findFragmentById(R.id.frag2);
+            }
+            float res= calRes(btId);
+            Log.i(" res in main: %s @@@@@@@@@@@@@@@", String.valueOf(res));
+            fragB.onNewClickSetResult(res);
+            //fragB.setOperandResult(op1, op2, btn.getText().toString(), zeroCnt);
+        }
+
+
+
+    private float calRes(int btn) {
+        String str_res = "";
+
+        switch (btn){
+            case R.id.btPlus:
+                initial_result = op1 + op2;
+                break;
+            case R.id.btMin:
+                initial_result = op1 - op2;
+                break;
+            case R.id.btMul:
+                initial_result = op1 * op2;
+                break;
+            case R.id.btDiv:
+                initial_result = op1 / op2;
+                break;
+            default:
+                break;
+        }
+        str_res = String.format("%." + zeroCnt + "f", initial_result);
+        Log.i(" calc res: %f", String.valueOf(initial_result));
+        return initial_result;
+    }
+
+    public boolean div_check(float ope2){
+        if(ope2 == 0) {
+            showToast("Divide by zero!!", getApplicationContext());
+            return false;
+        }
+        return true;
+    }
+
+    public boolean checkOperandExist(EditText operand1, EditText operand2){
+        if(operand1.getText().toString().isEmpty()){
+            showToast("fill operand1", getApplicationContext());
+            return false;
+        }
+        else if(operand2.getText().toString().isEmpty()){
+            showToast("fill operand2", getApplicationContext());
+            return false;
+        }
+        return true;
+    }
+
+    private void showToast(CharSequence str, Context context){
+        Toast toast = Toast.makeText(context, str, duration);
+        toast.show();
+    }
+
+}
